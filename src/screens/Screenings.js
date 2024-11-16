754b4b5ae2b434319f4a022260b4fb76bb875bec
Arttu Iivari <arttuiivari1@outlook.com>
2024-11-16 11:54:36 +0200

    basic theatreareas fetch made without components used yet

--- src/screens/Screenings.js
+++ src/screens/Screenings.js
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Screenings.css"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function Screenings(){
-    
+
+    const [areas, setAreas] = useState([]);
+
+    const getFinnkinoTheatres = (xml) =>{
+
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xml,"application/xml");
+        
+        const root = xmlDoc.children
+        
+        const theatres = root[0].children
+        const tempAreas = []
+        for(let i = 0;i< theatres.length;i++){
+            tempAreas.push(
+                {
+                "id": theatres[i].children[0].innerHTML,
+                "name": theatres[i].children[1].innerHTML    
+                }
+            )
+        }
+        setAreas(tempAreas);
+    }
+
+    useEffect(() =>{
+        fetch("https://www.finnkino.fi/xml/TheatreAreas/")
+        .then(response => response.text())
+        .then(xml => {
+            console.log(xml);
+            getFinnkinoTheatres(xml);
+        })
+        .catch(error =>
+        {
+            console.log(error);
+        }
+        )
+
+    }, [])
+
+
     return(
         <div className="Screenings">
             <Navbar />
-
+                <div>
+                    <select>
+                        {
+                            areas.map(area => (
+                                <option>{area.name}</option>
+                            ))
+                        }
+                    </select>
+                </div>
 
             <Footer />
         </div>
