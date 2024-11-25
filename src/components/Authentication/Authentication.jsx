fa2e5b05b1fa6575e98b30c68557d93e80d41576
Hikutaatti <niko.muikku618@gmail.com>
2024-11-25 13:41:42 +0200

    Form is up, formatting necessary, still needs to be setup with the database and actual implementation needed.

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Authentication.css'
 
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
-export const Authentication = () => {
+const Authentication = () => {
+
+  const [action, setAction] = useState('Log In')
+
   return (
     <div className = 'container'>
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
+        {action==="Log In"?<div></div>:<div className="input">
           <img src={user_icon} alt="" />
           <input type="text" placeholder="Name"/>
-        </div>
+        </div>}
         <div className="input">
           <img src={email_icon} alt=""  />
           <input type="email" placeholder="Email"/>
@@ -26,10 +29,10 @@ export const Authentication = () => {
           <input type="password" placeholder="Password"/>
         </div>
       </div>
-      <div className="forgot-password">Forgot Password? <span>Click Here</span></div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click Here</span></div>}
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Log In</div>
+        <div className={action==="Log In"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Log In")}}>Log In</div>
       </div>
     </div>
   )
