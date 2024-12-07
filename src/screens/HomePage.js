815d83f2c2c90a297f2b18928b30e31a349598e7
MuikkuMies <t3muni00@students.oamk.fi>
2024-12-07 04:41:02 +0200

    Modified formatting on the home page

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './Home.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import MovieCarousel from '../components/MovieCarousel';
 
-function HomePage() {
+const HomePage = () => {
+  const [username, setUsername] = useState(null);
+  
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+    setUsername(storedUsername);
+  } else {
+    setUsername("Guest");
+  }
+  }, []);
+
+
   return (
     <div className="Home">
       <Navbar />
       <div className='content'>
+        <p>Welcome</p>
+        <span>{username}</span>
       <MovieCarousel />
       </div>
       <Footer />
