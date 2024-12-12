ba5a66af067edb2f021f4349a135ee4168af0afd
MuikkuMies <t3muni00@students.oamk.fi>
2024-12-12 15:48:33 +0200

    Added username to profile page

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -4,24 +4,25 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FavoriteList from "../components/FavoriteList";
 import DeleteAccountButton from "../components/DeleteAccountButton";
+import { useUser } from "../contexts/useUser";
 
-
-
-function ProfilePage() {
+const ProfilePage = () => {
+  const { user } = useUser();
   return (
     <div className="ProfilePage">
       <Navbar />
-      <div class="container">
-  <h2>User Information</h2>
-  <p>Username: <span id="username"></span></p>
-  </div>
-  <div class="containerlistbutton">
-    <FavoriteList />
-    <DeleteAccountButton />
-  </div>
+      <div className="container">
+        <h2>User Information</h2>
+        <p>Username: {user.username}</p>
+      </div>
+      <div className="containerlistbutton">
+        <FavoriteList />
+        <DeleteAccountButton />
+      </div>
       <Footer />
     </div>
   );
+
 }
 
 export default ProfilePage;
