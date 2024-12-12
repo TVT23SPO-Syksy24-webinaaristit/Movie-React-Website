fc2a66cacd8d5976b08d686d13a93ff3d5cc8385
MuikkuMies <t3muni00@students.oamk.fi>
2024-12-12 15:50:45 +0200

    Revert "Added username to profile page"
    This reverts commit ba5a66af067edb2f021f4349a135ee4168af0afd.

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -4,25 +4,24 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FavoriteList from "../components/FavoriteList";
 import DeleteAccountButton from "../components/DeleteAccountButton";
-import { useUser } from "../contexts/useUser";
 
-const ProfilePage = () => {
-  const { user } = useUser();
+
+
+function ProfilePage() {
   return (
     <div className="ProfilePage">
       <Navbar />
-      <div className="container">
-        <h2>User Information</h2>
-        <p>Username: {user.username}</p>
-      </div>
-      <div className="containerlistbutton">
-        <FavoriteList />
-        <DeleteAccountButton />
-      </div>
+      <div class="container">
+  <h2>User Information</h2>
+  <p>Username: <span id="username"></span></p>
+  </div>
+  <div class="containerlistbutton">
+    <FavoriteList />
+    <DeleteAccountButton />
+  </div>
       <Footer />
     </div>
   );
-
 }
 
 export default ProfilePage;
